cbd2f491f4a837c650c270ccdbe0b0c8ff33fed4
Leo Oli <leooli_artist@outlook.com>
2022-12-05 09:03:40 -0300

    inicio do javascript cirado

--- minha-aplicacao-web/script/pedirlavagem.js
+++ minha-aplicacao-web/script/pedirlavagem.js
@@ -1,6 +1,38 @@
+// CABEÇALHO
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active')
 })
+// CABEÇALHO
+
+
+// CHECAR SE ESTÁ LOGADO
+// const script = document.createElement('script1');
+
+// script.onload = function entrar () {
+
+//   if (usuario === "logado") {
+//     let sair = document.getElementsByClassName("botao-sair")
+//     let entrar = "Entrar";
+//     sair.innerHTML = entrar;
+//   }
+
+//   console.log(entrar)
+
+// };
+// script.src = "./usuario.js";
+
+// document.head.appendChild(script);
+
+
+var usuario = false;
+const sair = document.querySelector('.botao-sair')
+
+
+
+if (usuario === true) {
+  sair.innerHTML = ("Sair");
+
+} else sair.innerHTML = ("Entrar");
