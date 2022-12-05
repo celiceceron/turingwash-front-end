1cab190e93c356ee2e9e1714d58730b5b39f7f48
Leo Oli <leooli_artist@outlook.com>
2022-12-05 07:43:17 -0300

    novos arquivos js adicionados

---
+++ minha-aplicacao-web/script/carros.js
@@ -0,0 +1,13 @@
+var carros = [{
+        "fabricante": "Nissan",
+        "nome": "Kicks",
+        "ano": "2018",
+        "logado": true
+    },
+    {
+        "fabricante": "Chevrolet",
+        "nome": "Onix",
+        "ano": "2015",
+        "logado": true
+    },
+]
