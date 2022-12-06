08e8acc02ca69fad05c4ada97eb94aff0fad6c3b
Leo Oli <leooli_artist@outlook.com>
2022-12-05 21:46:02 -0300

    teste feio

--- minha-aplicacao-web/script/pedirlavagem.js
+++ minha-aplicacao-web/script/pedirlavagem.js
@@ -26,3 +26,25 @@ script.src = "./script/usuario.js";
 
 document.head.appendChild(script);
 // CHECAR SE ESTÁ LOGADO
+
+
+
+// // DINAMICA DA LISTA DE LAVADORES
+// const script = document.createElement('script');
+
+// script.onload = function login() {
+
+//   const nome = document.querySelector('.card-title')
+//   const nota = document.querySelector('.card-nota')
+//   const avaliacoes = document.querySelector('#avaliacoes')
+//   const valor = document.querySelector('.valor')
+
+//   // if (usuario.logado === true) {
+//   //   sair.innerHTML = ("Sair");
+//   // } else sair.innerHTML = ("Entrar");
+
+// };
+
+// script.src = "./script/lavadores.js";
+
+// document.head.appendChild(script);
