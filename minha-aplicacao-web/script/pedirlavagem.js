d10993cb4276a9b69b4ae27bb8da02cd48b2b548
Leo Oli <leooli_artist@outlook.com>
2022-12-06 16:42:57 -0300

    lavadores

--- minha-aplicacao-web/script/pedirlavagem.js
+++ minha-aplicacao-web/script/pedirlavagem.js
@@ -30,21 +30,30 @@ document.head.appendChild(script);
 
 
 // // DINAMICA DA LISTA DE LAVADORES
-// const script = document.createElement('script');
+const script = document.createElement('script');
 
-// script.onload = function login() {
 
-//   const nome = document.querySelector('.card-title')
-//   const nota = document.querySelector('.card-nota')
-//   const avaliacoes = document.querySelector('#avaliacoes')
-//   const valor = document.querySelector('.valor')
+const nome = document.querySelector('.card-title')
+const nota = document.querySelector('.card-nota')
+const avaliacoes = document.querySelector('#avaliacoes')
+const valor = document.querySelector('.valor')
 
-//   // if (usuario.logado === true) {
-//   //   sair.innerHTML = ("Sair");
-//   // } else sair.innerHTML = ("Entrar");
 
-// };
 
-// script.src = "./script/lavadores.js";
+nome.innerHTML = lavadores[0].value;
+nota.innerHTML = lavadores[1].value;
+avaliacoes.innerHTML = lavadores[2].value;
+valor.innerHTML = lavadores[3].value;
 
-// document.head.appendChild(script);
+
+
+console.log({
+  nome,
+  nota,
+  avaliacoes,
+  valor,
+});
+
+script.src = "./script/lavadores.js";
+
+document.head.appendChild(script);
